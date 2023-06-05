9d4afff19cccbc96cc8aadff06355edf6f1e42f3
yihan12 <44987698+yihan12@users.noreply.github.com>
2023-06-05 15:01:38 +0800

    Update let.js

--- 第一章/let.js
+++ 第一章/let.js
@@ -2,3 +2,20 @@ let i;
 i = 0;
 i = i + 1;
 console.log(i); // i
+
+
+function f() {
+  {
+    let x;
+    {
+      // this is ok since it's a block scoped name
+      const x = "sneaky";
+      // error, was just defined with `const` above
+      x = "foo";
+    }
+    // this is ok since it was declared with `let`
+    x = "bar";
+    // error, already declared above in this block
+    let x = "inner";
+  }
+};
