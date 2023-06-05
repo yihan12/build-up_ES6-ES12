831efcfd657ca0246bbaa54d65eb5755ec82b016
yihan12 <44987698+yihan12@users.noreply.github.com>
2023-06-05 10:45:35 +0800

    Update const.js

--- 第一章/const.js
+++ 第一章/const.js
@@ -1,10 +1,12 @@
+// const 不变
 const j = 0;
 
-
+// const 内部变量属性可变
 const obj = { prop: 0 };
 obj.prop = obj.prop + 1;
 console.log(obj.prop); // 1
 
+// const 循环
 const arr = ['hello', 'world'];
 for (const elem of arr) {
   console.log(elem);
