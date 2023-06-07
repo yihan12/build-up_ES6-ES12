9d02e179b9a36aa119564894cf21c1ff86e71f1a
yihan12 <44987698+yihan12@users.noreply.github.com>
2023-06-07 10:34:05 +0800

    Update var.js

--- variable/var.js
+++ variable/var.js
@@ -7,3 +7,15 @@ console.log(window.objectVariable) // 'o'
 
 var number1 = 15 
 console.log(window.number1,globalThis.number1,window===globalThis); // 15 15 true
+
+function myFunc(){
+    var number1 = 15 // 函数作用域
+    if(true){
+        let number1 = 20 // 块级作用域
+        console.log(number1) // 20
+
+    }
+    console.log(number1) // 15
+}
+myFunc();
+console.log(number1) // number1 is not defined
